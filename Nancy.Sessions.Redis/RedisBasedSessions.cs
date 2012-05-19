c15f197a9d1f0cf63e4164d24757ddeb9c10335d
Chris Sainty <csainty@hotmail.com>
2012-05-19 15:06:07 +1000

    Added packages for Nancy and Redis with a basic layout of the class structure.

---
+++ Nancy.Sessions.Redis/RedisBasedSessions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy.Bootstrapper;
+
+namespace Nancy.Sessions.Redis
+{
+    public class RedisBasedSessions : IObjectSerializerSelector
+    {
+        IObjectSerializer serializer;
+
+        private static string cookieName = "_nsid";
+
+        public static IObjectSerializerSelector Enable(IPipelines pipelines)
+        {
+            var sessions = new RedisBasedSessions();
+            pipelines.BeforeRequest.AddItemToEndOfPipeline(LoadSession);
+            pipelines.AfterRequest.AddItemToEndOfPipeline(SaveSession);
+            return sessions;
+        }
+
+        private static Response LoadSession(NancyContext ctx)
+        {
+            return null;
+        }
+
+        private static void SaveSession(NancyContext ctx)
+        {
+            if (ctx.Request == null || ctx.Request.Session == null || !ctx.Request.Session.HasChanged)
+                return;
+        }
+
+        public void WithSerializer(IObjectSerializer newSerializer)
+        {
+            serializer = newSerializer;
+        }
+    }
+}
