0a4fc056c2d39268302883b66cfdcf483b9881d6
Chris Sainty <csainty@hotmail.com>
2012-05-19 18:10:25 +1000

    Implemented RedisSessionStore with integration tests which a server url is provided in App.config

---
+++ Nancy.Redis.Tests/Integration/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using ServiceStack.Redis;
+using Xunit;
+
+namespace Nancy.Redis.Tests.Integration
+{
+    public class Tests
+    {
+        [Fact]
+        public void CanLoadData()
+        {
+            string cs = ConfigurationManager.AppSettings["RedisServer"];
+            if (String.IsNullOrEmpty(cs))
+                return;
+
+            // Check we don't crash when session is missing
+            using (var client = new RedisClient(new Uri(cs)))
+            {
+                var store = new RedisSessionStore(client);
+                var data = store.Load<IDictionary<string, object>>("foo");
+                Assert.Null(data);
+            }
+            // Check we can save and reload some data
+            using (var client = new RedisClient(new Uri(cs)))
+            {
+                var store = new RedisSessionStore(client);
+                store.Save<IDictionary<string, object>>("Test", new Dictionary<string, object> { { "Key", "Value" }, { "Key2", "Value2" } });
+                var data = store.Load<IDictionary<string, object>>("Test");
+                Assert.Equal(2, data.Count);
+                Assert.Equal("Value", data["Key"]);
+                Assert.Equal("Value2", data["Key2"]);
+            }
+
+            // Test we can load an object from another connection/store isntance
+            using (var client = new RedisClient(new Uri(cs)))
+            {
+                var store = new RedisSessionStore(client);
+                var data = store.Load<IDictionary<string, object>>("Test");
+                Assert.Equal(2, data.Count);
+                Assert.Equal("Value", data["Key"]);
+                Assert.Equal("Value2", data["Key2"]);
+            }
+        }
+    }
+}
---
+++ Nancy.Redis/RedisSessionStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace Nancy.Redis
+{
+    public class RedisSessionStore : IKeyValueStore
+    {
+        private readonly IRedisClient client;
+        private readonly TimeSpan sessionLifetime;
+        private readonly string keyPrefix;
+
+        public RedisSessionStore(IRedisClient client) : this(client, "nancy-session:", TimeSpan.FromMinutes(20)) { }
+
+        public RedisSessionStore(IRedisClient client, string keyPrefix, TimeSpan sessionLifetime)
+        {
+            this.client = client;
+            this.sessionLifetime = sessionLifetime;
+            this.keyPrefix = keyPrefix;
+        }
+
+        public T Load<T>(string key)
+        {
+            key = FormatKey(key);
+            if (!client.ContainsKey(key))
+                return default(T);
+            client.ExpireEntryIn(key, sessionLifetime);
+            return client.Get<T>(key);
+        }
+
+        public void Save<T>(string key, T value)
+        {
+            client.Set(FormatKey(key), value, sessionLifetime);
+        }
+
+        private string FormatKey(string key)
+        {
+            return keyPrefix + key;
+        }
+    }
+}
