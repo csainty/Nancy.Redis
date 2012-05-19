a33d73422cbfc74a85cb6eb1151ccbbf93410898
Chris Sainty <csainty@hotmail.com>
2012-05-19 15:12:48 +1000

    Added test project.

---
+++ Nancy.Sessions.Redis.Tests/When_enabling_sessions.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Nancy.Testing;
+using Xunit;
+
+namespace Nancy.Sessions.Redis.Tests
+{
+    public class When_enabling_sessions
+    {
+        private ConfigurableBootstrapper _Bootstrapper;
+
+        public When_enabling_sessions()
+        {
+            _Bootstrapper = new ConfigurableBootstrapper(with =>
+            {
+                with.DisableAutoRegistration();
+            });
+            RedisBasedSessions.Enable(_Bootstrapper);
+        }
+
+        [Fact]
+        public void It_should_register_a_before_request_hook()
+        {
+            Assert.Equal(1, _Bootstrapper.BeforeRequest.PipelineItems.Count());
+        }
+
+        [Fact]
+        public void It_should_register_an_after_request_hook()
+        {
+            Assert.Equal(1, _Bootstrapper.AfterRequest.PipelineItems.Count());
+        }
+    }
+}
